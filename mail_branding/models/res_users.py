e867e1bc7293bb12b765dd1084ac663ad0d6ab79
solomonprabu <solomon.workspace@gmail.com>
2026-05-26 13:04:57 +0530

    Adapted to field inheritance method to change value to Curq

--- mail_branding/models/res_users.py
+++ mail_branding/models/res_users.py
@@ -1,23 +1,14 @@
-from odoo import api, models
+from odoo import fields, models
 
 
 class ResUsersPreferences(models.Model):
     _inherit = "res.users"
 
-    @api.model
-    def fields_get(self, allfields=None, attributes=None):
-        """
-        Modify the labels of:
-            - odoobot_state: OdooBot Status to CurqBot Status
-            - notification_type: Notification Selection label to Curq from Odoo.
-        """
-        res = super().fields_get(allfields, attributes)
-        if "odoobot_state" in res:
-            res["odoobot_state"]["string"] = "CurqBot Status"
-        if "notification_type" in res:
-            choices = self._fields["notification_type"]._description_selection(self.env)
-            new_choices = []
-            for choice in choices:
-                new_choices.append((choice[0], choice[1].replace("Odoo", "Curq")))
-            res["notification_type"]["selection"] = new_choices
-        return res
+    notification_type = fields.Selection(
+        selection_add=[("inbox", "In Curq")],
+        help="Policy on how to handle Chatter notifications:\n"
+        "- By Emails: notifications are sent to your email address\n"
+        "- In Curq: notifications appear in your Curq Inbox",
+    )
+
+    odoobot_state = fields.Selection(string="CurqBot Status")
