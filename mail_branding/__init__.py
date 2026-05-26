8689287573a786e614efe6f32b39980f218a89f1
solomonprabu <solomon.workspace@gmail.com>
2026-05-26 15:03:58 +0530

    [FIX] Resolving issues from review

--- mail_branding/__init__.py
+++ mail_branding/__init__.py
@@ -3,16 +3,14 @@ import base64
 from odoo.tools.misc import file_path
 
 
-def update_curqbot_name_in_res_users(env):
+def update_system_user(env):
     curq_icon_base64 = base64.b64encode(
         open(file_path("mail_branding/static/img/curqbot.png"), "rb").read()
     ).decode("utf-8")
     root_user_id = env.ref("base.user_root", raise_if_not_found=False)
     if root_user_id:
-        root_user_id.with_context(mail_notrack=True).write({"name": "CurqBot"})
-        # Change image of root user
         root_user_id.with_context(mail_notrack=True).write(
-            {"image_1920": curq_icon_base64}
+            {"name": "CurqBot", "image_1920": curq_icon_base64}
         )
 
 
@@ -27,9 +25,8 @@ def uninstall_hook(env):
     ).decode("utf-8")
     root_user_id = env.ref("base.user_root", raise_if_not_found=False)
     if root_user_id:
-        root_user_id.with_context(mail_notrack=True).write({"name": "OdooBot"})
         root_user_id.with_context(mail_notrack=True).write(
-            {"image_1920": odoo_icon_base64}
+            {"name": "OdooBot", "image_1920": odoo_icon_base64}
         )
     # Find the main company record (base.main_company)
     main_company = env.ref("base.main_company", raise_if_not_found=False)
